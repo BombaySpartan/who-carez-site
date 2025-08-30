b61ba8431d8d3c3af87a9e784a258b5a00bda399
BombaySpartan <khuzaan@gmail.com>
2025-08-30 16:04:38 -0500

    changes were made

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Who Carez!</h1>
+      <p>This is a non-profit platform encouraging people to rise up and start caring.</p>
+    </div>
+  );
+}
+
+export default App;
